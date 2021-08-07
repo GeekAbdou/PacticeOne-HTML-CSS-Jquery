688325e4c9c1345c2752f3d60f317568444187b5
Abd-Elrahman Elsayed <abd.hassan98@eng-st.cu.edu.eg>
2021-08-07 04:10:20 +0200

    silder

---
+++ assets/js/JQuery_main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  "use strict";
+  $(".bgimg").height($(window).height());
+  $(".bgimg").width($(window).width());
+
+  $(window).resize(function () {
+    $(".bgimg").height($(window).height());
+    $(".bgimg").width($(window).width());
+  });
+  /*
+  $(".links li").click(function () { 
+      $(this).addClass('active').siblings().removeClass('active');
+  });
+*/
+  $(".links li").hover(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+  //Slider Trigger
+  $(".slider").bxSlider({
+    pager: false,
+  });
+  $(".bgimg .slider li").each(function () {
+    $(this).css("padding-top",20);
+  });
+// "padding-top",($(window).height() - $(".slider li").height()) / 2
+});
